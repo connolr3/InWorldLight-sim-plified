2cfc66b475fe2e20842ca270ba59e434119ce070
Rosie Connolly <connolr3@tcd.ie>
2024-02-16 16:03:46 +0000

    cganged ui images

---
+++ Assets/MyExperimentRunner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyExperimentRunner : MonoBehaviour
+{
+    public string[] AIGender;
+    public string[] Locomotion;
+    public int TrialRepetitions;
+
+    private int currentTrial=0;
+     private int currentBlock=0;
+       private int currentTrialBlock=0;
+
+       void Start(){
+
+
+       }
+}
